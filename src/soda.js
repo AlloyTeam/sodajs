d134d3c6eba37fb1e10bd4c1fdb859b4a3fc4e38
cjd6568358 <cjd6568358@163.com>
2017-05-19 19:23:30 +0800

    fix 添加数组forEach原型方法

--- src/soda.js
+++ src/soda.js
@@ -34,6 +34,32 @@
         };
     }
 
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function (callback) {
+            var T, k;
+            if (this == null) {
+                throw new TypeError('this is null or not defined');
+            }
+            var O = Object(this);
+            var len = O.length >>> 0;
+            if (typeof callback !== 'function') {
+                throw new TypeError(callback + ' is not a function');
+            }
+            if (arguments.length > 1) {
+                T = arguments[1];
+            }
+            k = 0;
+            while (k < len) {
+                var kValue;
+                if (k in O) {
+                    kValue = O[k];
+                    callback.call(T, kValue, k, O);
+                }
+                k++;
+            }
+        };
+    }
+
     if (!String.prototype.trim) {
         String.prototype.trim = function () {
             return this.replace(/^\s*|\s*$/g, '');
