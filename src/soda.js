9850f92565274a13ca2580b499a27cd6894b7c0f
cjd6568358 <cjd6568358@163.com>
2017-06-21 17:02:03 +0800

    fix ie 8 9 原型链属性无法设置

--- src/soda.js
+++ src/soda.js
@@ -60,6 +60,48 @@
         };
     }
 
+    if (typeof Object.create != 'function') {
+        Object.create = (function() {
+            //为了节省内存，使用一个共享的构造器
+            function Temp() {}
+
+            // 使用 Object.prototype.hasOwnProperty 更安全的引用 
+            var hasOwn = Object.prototype.hasOwnProperty;
+
+            return function(O) {
+                // 1. 如果 O 不是 Object 或 null，抛出一个 TypeError 异常。
+                if (typeof O != 'object') {
+                    throw TypeError('Object prototype may only be an Object or null');
+                }
+
+                // 2. 使创建的一个新的对象为 obj ，就和通过
+                //    new Object() 表达式创建一个新对象一样，
+                //    Object是标准内置的构造器名
+                // 3. 设置 obj 的内部属性 [[Prototype]] 为 O。
+                Temp.prototype = O;
+                var obj = new Temp();
+                Temp.prototype = null; // 不要保持一个 O 的杂散引用（a stray reference）...
+
+                // 4. 如果存在参数 Properties ，而不是 undefined ，
+                //    那么就把参数的自身属性添加到 obj 上，就像调用
+                //    携带obj ，Properties两个参数的标准内置函数
+                //    Object.defineProperties() 一样。
+                if (arguments.length > 1) {
+                    // Object.defineProperties does ToObject on its first argument.
+                    var Properties = Object(arguments[1]);
+                    for (var prop in Properties) {
+                        if (hasOwn.call(Properties, prop)) {
+                            obj[prop] = Properties[prop];
+                        }
+                    }
+                }
+
+                // 5. 返回 obj
+                return obj;
+            };
+        })();
+    }
+
     if (!String.prototype.trim) {
         String.prototype.trim = function() {
             return this.replace(/^\s*|\s*$/g, '');
@@ -455,15 +497,10 @@
                     var itemNode = el.cloneNode(true);
 
                     // 这里创建一个新的scope
-                    var itemScope = {};
+                    var itemScope = Object.create(scope)
                     itemScope[trackName] = i;
 
                     itemScope[itemName] = repeatObj[i];
-
-                    itemScope.__proto__ = scope;
-
-                    // REMOVE cjd6568358
-                    // itemNode.removeAttribute(prefix + '-repeat');
 
                     el.parentNode.insertBefore(itemNode, el);
 
